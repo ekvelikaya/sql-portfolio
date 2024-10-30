7ca56d48f1f5e2e7937d5db2e99aaf30e1f70abc
Ekaterina <velikayaek@icloud.com>
2024-10-30 16:51:50 +0400

    add tasks

--- leetcode-sql-50/string functions-regex-clause.sql
+++ leetcode-sql-50/string functions-regex-clause.sql
@@ -33,3 +33,13 @@ FROM
     users
 ORDER BY 
     user_id;
+
+-- Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. 
+-- Type I Diabetes always starts with DIAB1 prefix.
+
+SELECT patient_id,
+		 patient_name,
+		 conditions
+FROM Patients
+WHERE conditions LIKE '% DIAB1%'
+		OR conditions LIKE 'DIAB1%';
