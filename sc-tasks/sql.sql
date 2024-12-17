4f79036aa1e0836205993243cd717c6231a28d1c
Ekaterina <velikayaek@icloud.com>
2024-12-18 01:49:16 +0200

    upd

--- sc-tasks/sql.sql
+++ sc-tasks/sql.sql
@@ -1,13 +1,11 @@
-
-
+-- 1.
 SELECT bike_number,
 		 max(end_time) AS last_time
 FROM dc_bikeshare_q1_2012
 GROUP BY  bike_number
 ORDER BY  last_time desc;
 
-
-
+-- 2.
 SELECT worker_title
 FROM worker w
 LEFT JOIN title t
@@ -16,23 +14,20 @@ WHERE salary =
 	(SELECT max(salary)
 	FROM worker);
 
-
-
+-- 3.
 SELECT count(worker_id)
 FROM worker
 WHERE joining_date >= '2014-04-01'
 GROUP BY  department
 HAVING department='Admin';
 
-
-
+-- 4.
 SELECT company,
 		 profits
 FROM forbes_global_2010_2014
 ORDER BY  profits DESC limit 3;
 
-
-
+-- 5.
 with t1 AS 
 	(SELECT user_id,
 		 min(timestamp) AS first_time,
@@ -54,7 +49,7 @@ JOIN t2
 		AND t1.d1=t2.d2
 GROUP BY  t1.user_id;
 
-
+-- 6.
 SELECT from_user,
 		 count(*) AS total_emails,
 		 row_number() over(order by count(*) desc,
@@ -62,9 +57,7 @@ SELECT from_user,
 FROM google_gmail_emails
 GROUP BY  from_user;
 
-
-
-
+-- 7.
 SELECT DISTINCT user_id from
 	(SELECT user_id,
 		 lead(created_at) over(partition by user_id
@@ -72,9 +65,7 @@ SELECT DISTINCT user_id from
 	FROM amazon_transactions ) t1
 WHERE days<=7; 
 
-
-
-
+-- 8.
 with t1 AS 
 	(SELECT count(product_name) AS a_2019_count,
 		 company_name
@@ -95,16 +86,13 @@ JOIN t2
 	ON t1.company_name=t2.company_name;
 
 
-
-
+-- 9.
 SELECT max(salary) filter(where d.department='marketing')-max(salary) filter(where d.department='engineering')
 FROM db_employee e
 JOIN db_dept d
 	ON e.department_id=d.id; 
 
-
-
-
+-- 10.
 SELECT id,
 		 first_name,
 		 last_name,
@@ -114,9 +102,7 @@ FROM ms_employee_salary
 GROUP BY  id, first_name, last_name, department_id
 ORDER BY  id asc;
 
-
-
-
+-- 11.
 SELECT hotel_name,
 		 reviewer_score,
 		 count(reviewer_score)
@@ -124,16 +110,12 @@ FROM hotel_reviews
 WHERE hotel_name = 'Hotel Arena'
 GROUP BY  hotel_name, reviewer_score;
 
-
-
-
+-- 12.
 SELECT count(distinct movie)
 FROM oscar_nominees
 WHERE nominee = 'Abigail Breslin';
 
-
-
-
+-- 13.
 SELECT post_id,
 		 poster,
 		 post_text,
